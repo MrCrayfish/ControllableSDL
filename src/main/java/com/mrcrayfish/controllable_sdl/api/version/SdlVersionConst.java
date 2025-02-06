b6f79d8501f12ba702f2a8a4c16a1426702330cc
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-02-06 23:50:16 +1030

    🐛 Fix incorrect patch level

--- src/main/java/com/mrcrayfish/controllable_sdl/api/version/SdlVersionConst.java
+++ src/main/java/com/mrcrayfish/controllable_sdl/api/version/SdlVersionConst.java
@@ -4,7 +4,7 @@ public final class SdlVersionConst {
 
     public static final int SDL_MAJOR_VERSION = 2;
     public static final int SDL_MINOR_VERSION = 30;
-    public static final int SDL_PATCHLEVEL = 10;
+    public static final int SDL_PATCHLEVEL = 12;
 
     private SdlVersionConst() {
     }
