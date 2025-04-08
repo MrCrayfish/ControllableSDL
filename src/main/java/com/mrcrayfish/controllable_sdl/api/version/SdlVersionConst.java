04ce0d8837cc19d4dd9d7fee51eaa4c2842d5ba1
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-04-08 19:09:32 +0930

    🐛 Fix version constant

--- src/main/java/com/mrcrayfish/controllable_sdl/api/version/SdlVersionConst.java
+++ src/main/java/com/mrcrayfish/controllable_sdl/api/version/SdlVersionConst.java
@@ -3,8 +3,8 @@ package com.mrcrayfish.controllable_sdl.api.version;
 public final class SdlVersionConst {
 
     public static final int SDL_MAJOR_VERSION = 2;
-    public static final int SDL_MINOR_VERSION = 30;
-    public static final int SDL_PATCHLEVEL = 12;
+    public static final int SDL_MINOR_VERSION = 32;
+    public static final int SDL_PATCHLEVEL = 4;
 
     private SdlVersionConst() {
     }
